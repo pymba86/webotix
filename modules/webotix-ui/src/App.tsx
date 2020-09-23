c3701f46a4cf2f920507846119588ee3ccbe80fd
Artem Matveychuk <pymba1996@live.com>
2020-09-23 23:54:25 +0500

    Подлючение react библиотек

--- modules/webotix-ui/src/App.tsx
+++ modules/webotix-ui/src/App.tsx
@@ -1,27 +1,6 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
-
-interface ContainerProps {
-    isFilled: boolean;
-}
-
-declare module 'styled-components' {
-    export interface DefaultTheme {
-        borderRadius: string
-
-        colors: {
-            main: string
-            secondary: string
-        }
-    }
-}
-
-const Container = styled.div<ContainerProps>`
-  border-radius: 10px;
-  background: ${({theme}) => theme.colors.main};
-  ${({isFilled, theme}) => isFilled && css`
-    border: ${theme.colors.main}`};
-`
+import {Link} from './elements/Link';
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
     return (
@@ -30,14 +9,11 @@ function App() {
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload!@#.
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <BrowserRouter>
+                    <Link to={'#'} color={"red"} m={3} fontSize={4}>
+                        Ссылка
+                    </Link>
+                </BrowserRouter>
             </header>
         </div>
     );
--- modules/webotix-ui/src/elements/Button.tsx
+++ modules/webotix-ui/src/elements/Button.tsx
@@ -1,12 +1,2 @@
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 
-interface ButtonProps {
-    color: string;
-}
-
-const ButtonBase = styled.button<ButtonProps>`
-    color: ${({color}) => color}
-`;
-
-const Button = styled(ButtonBase).attrs<ButtonProps>(
-    {color: "123"})(ButtonBase);
